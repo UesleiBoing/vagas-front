c0ab7d9747575061353d9863ee39c051c520577a
Afonso <boingfueslei@hotmail.com>
2022-06-29 19:23:15 -0300

    Card azul

--- src/components/views/HomePadrao.vue
+++ src/components/views/HomePadrao.vue
@@ -31,11 +31,11 @@
 
     <div class="row mt-5">
       <div class="col-4">
-        <indicador-padrao titulo="Vagas abertas" indicador="100" bg="bg-dark" color="text-white"></indicador-padrao>
+        <indicador-padrao titulo="Vagas abertas" indicador="100" bg="bg-info" color="text-white"></indicador-padrao>
       </div>
 
       <div class="col-4">
-        <indicador-padrao titulo="Profissionais Cadastrados" indicador="254" bg="bg-dark" color="text-white"></indicador-padrao>
+        <indicador-padrao titulo="Profissionais Cadastrados" indicador="254" bg="bg-info" color="text-white"></indicador-padrao>
       </div>
 
       <div class="col-4">
