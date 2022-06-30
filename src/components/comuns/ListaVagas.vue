0c14fc408b76b690e1cc89b4b88ef1f1a9bdea31
UesleiBoing <67610805+UesleiBoing@users.noreply.github.com>
2022-06-29 21:27:47 -0300

    Update ListaVagas.vue

--- src/components/comuns/ListaVagas.vue
+++ src/components/comuns/ListaVagas.vue
@@ -23,7 +23,7 @@
 </template>
 <script>
 import VagaPadrao from '@/components/comuns/VagaPadrao.vue'
-export deault {
+expor deault {
     name : 'ListaVagas',
     data : () => ({
         vagas: []
